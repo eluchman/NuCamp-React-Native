2345946dbbc0742617e6ec417af333af196a812d
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-01-18 13:52:43 -0500

    Gesture Update

--- CampsiteInfoComponent.js
+++ CampsiteInfoComponent.js
@@ -20,17 +20,54 @@ const mapDispatchToProps = {
     postComment: (campsiteId, rating, author, text, comment) => (postComment(campsiteId, rating, author, text, comment))
 };
 
-function RenderCampsite({props}) {
-   
-    const {campsite} = props;
+
     
     
 function RenderCampsite(props) {
+
+    
 
     const {campsite} = props;
 
     handleViewRef = ref => this.view = ref;
 
+    const recognizeComment = ({dx}) => (dx < -200) ? true : false;
+
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log('pan responder end', gestureState);
+            if (recognizeComment(gestureState)) {
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + campsite.name + ' to favorites?',
+                    [
+                        {
+                            text: 'Cancel',
+                            style: 'cancel',
+                            onPress: () => console.log('Cancel Pressed')
+                        },
+                        {
+                            text: 'OK',
+                            onPress: () => props.favorite ?
+                                console.log('Already set as a favorite') : props.markFavorite()
+                        }
+                    ],
+                    { cancelable: false }
+                );
+            }
+            return true;
+        }
+    });
+
+
+    //recognizeDrag
+    
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
     const panResponder = PanResponder.create({
@@ -263,5 +300,6 @@ const styles = StyleSheet.create({
         margin: 20
     }
 });
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
