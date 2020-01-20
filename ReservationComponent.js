36896c3d51056392194e70bc9a8713fe96a5bd87
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-01-20 14:27:56 -0500

    Add files via upload
    Local notifications update

--- ReservationComponent.js
+++ ReservationComponent.js
@@ -62,6 +62,28 @@ class Reservation extends Component {
         });
     }
 
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
+    }
+
     render() {
         return (
             <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
