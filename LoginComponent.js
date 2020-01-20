c6c7fcf80ad91cb415f3026b42dcfd03362faee9
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-01-20 11:34:24 -0500

    Add files via upload
    Login Component

---
+++ LoginComponent.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+import { Input, CheckBox } from 'react-native-elements';
+import * as SecureStore from 'expo-secure-store';
