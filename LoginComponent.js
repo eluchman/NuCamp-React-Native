b557415ac0383aaa6c53f4446d2b75e1489fe092
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-01-20 11:35:26 -0500

    Add files via upload
    Login Component Update

--- LoginComponent.js
+++ LoginComponent.js
@@ -2,3 +2,19 @@ import React, { Component } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
 import { Input, CheckBox } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
+
+class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            password: '',
+            remember: false
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Login'
+    }
