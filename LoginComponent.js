7b8a2e47e34fc1fca00cf2dedfd1216e81b891b6
Eric W Luchman <48758468+eluchman@users.noreply.github.com>
2020-01-21 10:55:35 -0500

    Add files via upload
    Registration Form Update

--- LoginComponent.js
+++ LoginComponent.js
@@ -143,7 +143,7 @@ class RegisterTab extends Component {
         )
     }
 
-    handleLogin() {
+    handleRegister() {
         console.log(JSON.stringify(this.state));
         if (this.state.remember) {
             SecureStore.setItemAsync('userinfo', JSON.stringify(
@@ -159,7 +159,81 @@ class RegisterTab extends Component {
     render() {
         return(
             <ScrollView>
-
+                <View style={styles.container}>
+                    <View style={styles.imageContainer}>
+                        <Image
+                            source={{uri: this.state.imageUrl}}
+                            loadingIndicatorSource={require('./images/logo.png')}
+                            style={styles.image}
+                        />
+                        <Button
+                            title='Camera'
+                            onPress={this.getImageFromCamera}
+                        />
+                    </View>
+                    <Input
+                        placeholder='Username'
+                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                        onChangeText={username => this.setState({username})}
+                        value={this.state.username}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder='Password'
+                        leftIcon={{type: 'font-awesome', name: 'key'}}
+                        onChangeText={password => this.setState({password})}
+                        value={this.state.password}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder='First Name'
+                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                        onChangeText={firstname => this.setState({firstname})}
+                        value={this.state.firstname}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder='Last Name'
+                        leftIcon={{type: 'font-awesome', name: 'user-o'}}
+                        onChangeText={lastname => this.setState({lastname})}
+                        value={this.state.lastname}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <Input
+                        placeholder='Email'
+                        leftIcon={{type: 'font-awesome', name: 'envelope-o'}}
+                        onChangeText={email => this.setState({email})}
+                        value={this.state.email}
+                        containerStyle={styles.formInput}
+                        leftIconContainerStyle={styles.formIcon}
+                    />
+                    <CheckBox
+                        title='Remember Me'
+                        center
+                        checked={this.state.remember}
+                        onPress={() => this.setState({remember: !this.state.remember})}
+                        containerStyle={styles.formCheckbox}
+                    />
+                    <View style={styles.formButton}>
+                        <Button
+                            onPress={() => this.handleRegister()}
+                            title='Register'
+                            icon={
+                                <Icon
+                                    name='user-plus'
+                                    type='font-awesome'
+                                    color='#fff'
+                                    iconStyle={{marginRight: 10}}
+                                />
+                            }
+                            buttonStyle={{backgroundColor: '#5637DD'}}
+                        />
+                    </View>
+                </View>
             </ScrollView>
         );
     }
